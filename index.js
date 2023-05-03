52450875860dd41b3a39c4cb0b20e26235318fac
Abu Bokor <abubokor1066@gmail.com>
2023-05-03 15:19:49 +0600

    chef json added

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
+const chef = require("./chef.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chef);
 });
 
 app.listen(port, () => {
